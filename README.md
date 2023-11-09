8996c2390e3c9f09865029876bd751dd3c03cd6f
Ed Cottrell <github@edcottrell.com>
2023-11-08 22:22:38 -0500

    Refactored to allow using this as either an included file (via a class) or via the command line. Added better error handling and basic security checks.

--- README.md
+++ README.md
@@ -1,35 +1,49 @@
-This is a simple command-line tool to slice an image into a grid using PHP. It relies on core PHP functionality and the Imagick PHP extension but has no other dependencies.
+This is a simple class and command-line tool to slice an image into a grid using PHP. It relies on core PHP functionality and the [Imagick](https://github.com/Imagick/imagick) PHP extension but has no other dependencies.
 
 # Usage
 
-The tool can be invoked in two ways:
+The tool can be invoked in three ways:
 
-```bash
-# 1) Using a command-file-rows-columns syntax:
-# php image-grid-slicer.php image_file rows columns 
-#
-# Example:
+1. By including it and using the class in a PHP application, as in the following:
 
-php image-grid-slicer.php image.png 3 4 
+    ```php
+    include 'image-grid-slicer.php';
+    $srcImagePath = 'ham.gif';
+    $rows = 2;
+    $columns = 2;
+    $slicer = new phpImageGridSlicer($srcImagePath, $rows, $columns);
+    $slicer->slice();
+    ```
 
-# 2) Using parameters:
-# php image-grid-slicer.php image_file --rows rows --columns columns 
-# OR
-# php image-grid-slicer.php image_file --columns columns --rows rows
-#
-# Examples:
+2. On the command line, using a syntax consisting simply of the file name, the number of rows, and the number of columns:
+    ```bash
+    # This method uses a command-file-rows-columns syntax:
+    # php image-grid-slicer.php image_file rows columns 
+    #
+    # Example:
+    
+    php image-grid-slicer.php image.png 3 4
+    ```
+3. On the command line, using named parameters:
+    ```bash
+    # php image-grid-slicer.php image_file --rows rows --columns columns 
+    # OR
+    # php image-grid-slicer.php image_file --columns columns --rows rows
+    #
+    # Examples:
+    
+    php image-grid-slicer.php image.png --rows 3 --columns 4
+    php image-grid-slicer.php image.png --columns 6 --rows 1
+    ```
 
-php image-grid-slicer.php image.png --rows 3 --columns 4
-php image-grid-slicer.php image.png --columns 6 --rows 1
-```
+Files are automatically named as `{$BASE_FILE_NAME}_slice_r_{$ROW}_c_{$COLUMN}.{$ORIGINAL_EXTENSION}`, *e.g.*, `My Image_slice_r2_c3.png`.
 
-Files are automatically named as *BASE\_FILE\_NAME*\_slice\_r\_*row*\_c\_*column*.*original_extension*, *e.g.*, `My Image_slice_r2_c3.png`.
+The tool will throw `ErrorException`s for invalid parameters or if the file specified does not appear to be an image based on its MIME type or extension. It will not throw any other type of exception or error directly, but it will pass through `ImagickException`s or any other exceptions thrown by Imagick. 
 
 # Future Plans
 
 My plans for this library depend on what users ask for, any pull requests I receive, and my own needs. In general, though, I'll likely prioritize the following features:
 
-- wrap this up in a function so that it can be used as either a command line tool or an add-in in other projects
 - specify a different output directory (right now, it saves slices to the same directory as the original image)
 - specify a different output file naming format (prefix and suffix, like `My Image 7` for sequentially-numbered slices)
 - specify an output file format (.jpeg to .png, for example)
--- image-grid-slicer.php
+++ image-grid-slicer.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace ImageGridSlicer;
+
+use ErrorException;
+use Imagick;
+use ImagickException;
+
 /**
  * Ed Cottrell's tool for slicing an image along a grid.
  * (c) 2022 Ed Cottrell
@@ -11,65 +17,144 @@
  * alternative output formats, custom naming schemes, etc.
  */
 
-$srcImagePath = '';
-$rows = 0;
-$columns = 0;
+class ImageGridSlicer {
+    private string $sliceBaseFileName;
+    public string $srcImagePath;
+    public string $srcImageFileName;
+    public int $rows = 0;
+    public int $columns = 0;
+    private Imagick $imagickObj;
 
-if ($argc <= 1) {
-    throw new ErrorException("You must specify an image, the number of rows, and the number of columns");
-}
-$arguments = join(" ", array_slice($argv, 1));
-$matched = preg_match('/^(?P<srcImagePath>.+) +(?P<rows>\d+) +(?P<columns>\d+)$/i', $arguments, $matches);
-if ($matched) {
-    $srcImagePath = $matches['srcImagePath'];
-    $rows = $matches['rows'];
-    $columns = $matches['columns'];
-} else {
-    $matched = preg_match('/^(?P<srcImagePath>.+?) +--/i', $arguments, $matches);
-    if (!$matched) {
-        throw new ErrorException("You must specify the number of rows");
+    public function __construct(?string $srcImagePath, ?int $rows, ?int $columns)
+    {
+        $this->srcImagePath = $srcImagePath;
+        $this->rows = $rows;
+        $this->columns = $columns;
+        if ($this->srcImagePath) {
+            $this->loadImageFromFile($srcImagePath);
+        }
     }
-    $srcImagePath = $matches['srcImagePath'];
 
-    $matched = preg_match('/(?:--rows|-r) +(?P<rows>\d+)/i', $arguments, $matches);
-    if (!$matched) {
-        throw new ErrorException("You must specify the number of rows");
+    public function getSliceBaseFileName(): string
+    {
+        $this->sliceBaseFileName = preg_replace('/^(.+)\.(?:gif|jpe?g|png)$/i', '$1', $this->srcImageFileName);
+        return $this->sliceBaseFileName;
     }
-    $rows = $matches['rows'];
 
-    $matched = preg_match('/(?:--columns|-c) +(?P<columns>\d+)/i', $arguments, $matches);
-    if (!$matched) {
-        throw new ErrorException("You must specify the number of columns");
+    /**
+     * @throws ErrorException
+     * @throws ImagickException
+     */
+    public function loadImageFromFile(?string $srcImagePath) : Imagick
+    {
+        if (!empty($srcImagePath)) {
+            $this->srcImagePath = $srcImagePath;
+        }
+        $this->validateImageIsImage();
+
+        // Get Image File path
+        $this->srcImageFileName = preg_replace('/^(?:.+\/)?([^\/]+)$/i', '$1', $this->srcImagePath);
+        if (false === strpos($this->srcImagePath, "/")) {
+            $srcImagePath = __DIR__ . '/';
+        } else {
+            $srcImagePath = preg_replace('/^(.+\/)?(?:[^\/]+)$/i', '$1', $this->srcImagePath);
+        }
+
+        // Load the Image
+        $this->imagickObj = new Imagick($srcImagePath . $this->srcImageFileName);
+
+        return $this->imagickObj;
     }
-    $columns = $matches['columns'];
+
+    /** @noinspection PhpUnused */
+    public function loadImagickImage(Imagick $imagickObj) : Imagick
+    {
+        $this->imagickObj = $imagickObj;
+        $this->sliceBaseFileName = null;
+        $this->srcImagePath = null;
+        $this->srcImageFileName = null;
+        return $this->imagickObj;
+    }
+
+    /**
+     * @throws ImagickException
+     */
+    public function slice() : void {
+        // Figure out how to slice the image
+        $srcImageWidth = $this->imagickObj->getImageWidth();
+        $srcImageHeight = $this->imagickObj->getImageHeight();
+        $sliceWidth = $srcImageWidth / $this->columns;
+        $sliceHeight = $srcImageHeight / $this->rows;
+
+        $this->getSliceBaseFileName();
+
+        // Slice away
+        for ($row = 0; $row < $this->rows; $row++) {
+            for ($col = 0; $col < $this->columns; $col++) {
+                $new_imagick = $this->imagickObj->getImageRegion( (int) $sliceWidth,
+                    (int) $sliceHeight,
+                    (int) ($col * $sliceWidth),
+                    (int) ($row * $sliceHeight)
+                );
+                $new_imagick->writeImage($this->srcImagePath . "{$this->sliceBaseFileName}_slice_r{$row}_c$col.png");
+            }
+        }
+    }
+
+    /**
+     * @throws ErrorException
+     */
+    private function validateImageIsImage() : void {
+        if (!preg_match('/\.(gif|jpe?g|png)/i', $this->srcImagePath)) {
+            throw new ErrorException("Security Exception: File $this->srcImagePath does not appear to be an image file");
+        }
+
+        $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($fileInfo, $this->srcImagePath);
+        $allowedMimeTypes = [
+            'image/gif',
+            'image/jpeg',
+            'image/jpg',
+            'image/png'
+        ];
+        if (in_array($mimeType, $allowedMimeTypes) === false) {
+            throw new ErrorException("Security Exception: File $this->srcImagePath does not appear to be an image file");
+        }
+    }
 }
 
-if (!preg_match('/\.(gif|jpe?g|png)/i', $srcImagePath)) {
-    throw new ErrorException("The file specified does not appear to be an image");
-}
-
-$srcImageFileName = preg_replace('/^(?:.+\/)?([^\/]+)$/i', '$1', $srcImagePath);
-if (false === strpos($srcImagePath, "/")) {
-    $srcImagePath = __DIR__ . '/';
-} else {
-    $srcImagePath = preg_replace('/^(.+\/)?(?:[^\/]+)$/i', '$1', $srcImagePath);
-}
-
-$imagick = new Imagick($srcImagePath . $srcImageFileName);
-
-$sliceBaseName = preg_replace('/^(.+)\.(?:gif|jpe?g|png)$/i', '$1', $srcImageFileName);
-$srcImageWidth = $imagick->getImageWidth();
-$srcImageHeight = $imagick->getImageHeight();
-$sliceWidth = $srcImageWidth / $columns;
-$sliceHeight = $srcImageHeight / $rows;
-
-for ($row = 0; $row < $rows; $row++) {
-    for ($col = 0; $col < $columns; $col++) {
-        $new_imagick = $imagick->getImageRegion( (int) $sliceWidth,
-            (int) $sliceHeight,
-            (int) ($col * $sliceWidth),
-            (int) ($row * $sliceHeight)
-        );
-        $new_imagick->writeImage($srcImagePath . "{$sliceBaseName}_slice_r{$row}_c$col.png");
+// If called directly from the CLI, then process the arguments
+if (!empty($argv) && basename($argv[0]) === basename(__FILE__)) {
+    // Parse Command Line
+    if ($argc <= 1) {
+        throw new ErrorException("You must specify an image, the number of rows, and the number of columns");
     }
+    $arguments = join(" ", array_slice($argv, 1));
+    $matched = preg_match('/^(?P<srcImagePath>.+) +(?P<rows>\d+) +(?P<columns>\d+)$/i', $arguments, $matches);
+    if ($matched) {
+        $srcImagePath = $matches['srcImagePath'];
+        $rows = $matches['rows'];
+        $columns = $matches['columns'];
+    } else {
+        $matched = preg_match('/^(?P<srcImagePath>.+?) +--/i', $arguments, $matches);
+        if (!$matched) {
+            throw new ErrorException("You must specify the number of rows");
+        }
+        $srcImagePath = $matches['srcImagePath'];
+
+        $matched = preg_match('/(?:--rows|-r) +(?P<rows>\d+)/i', $arguments, $matches);
+        if (!$matched) {
+            throw new ErrorException("You must specify the number of rows");
+        }
+        $rows = $matches['rows'];
+
+        $matched = preg_match('/(?:--columns|-c) +(?P<columns>\d+)/i', $arguments, $matches);
+        if (!$matched) {
+            throw new ErrorException("You must specify the number of columns");
+        }
+        $columns = $matches['columns'];
+    }
+
+    $pigs = new ImageGridSlicer($srcImagePath, $rows, $columns);
+    $pigs->slice();
 }
